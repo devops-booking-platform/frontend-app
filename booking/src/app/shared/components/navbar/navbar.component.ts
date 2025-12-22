6c3198d6dd756c7a1e31b59c941b267b74898cd5
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-12-22 21:07:45 +0100

    feat(frontend): Slight refactor of design

--- booking/src/app/shared/components/navbar/navbar.component.ts
+++ booking/src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +10,10 @@ import { AuthService } from '../../../auth/services/auth.service';
 export class NavbarComponent {
   isLoggedIn$ = this.authService.isLoggedIn$;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   logout() {
     this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
