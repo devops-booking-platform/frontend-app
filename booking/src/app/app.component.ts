540dba1311b7a4b3c4a48073f180c3350a3ab62b
Nebojša Vuga <nebojsavuga01@gmail.com>
2026-01-04 20:29:27 +0100

    feat: remove unused imports

--- booking/src/app/app.component.ts
+++ booking/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { NotificationSignalRService } from './core/services/notification-signalr.service';
+import { Component } from '@angular/core';
 import { NotificationListenerService } from './core/services/notification-listener.service';
 
 @Component({
