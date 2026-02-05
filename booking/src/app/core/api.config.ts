da356f8e7fb37e00dd8786276b20e48c3478607d
Bogdan Janosevic <96696175+BogdanJ33@users.noreply.github.com>
2026-02-05 23:12:23 +0100

    feat(ingress): Changed api config so it works with ingress (#4)

--- booking/src/app/core/api.config.ts
+++ booking/src/app/core/api.config.ts
@@ -1,11 +1,12 @@
 export const ApiConfig = {
-    userService: 'http://localhost:5001/api',
-    accommodationService: 'http://localhost:5002/api',
-    ratingService: 'http://localhost:5003/api',
-    reservationService: 'http://localhost:5004/api',
-    notificationService: 'http://localhost:5006/api',
-    notificationServiceHub: 'http://localhost:5006/notificationHub',
-    searchService: 'http://localhost:5005/api',
-    cloudinaryCloudName: 'du4qudomo',
-    cloudinaryUploadPresetName: 'booking-platform'
+  userService: '/api/user/api',
+  accommodationService: '/api/accommodation/api',
+  ratingService: '/api/rating/api',
+  reservationService: '/api/reservation/api',
+  notificationService: '/api/notification/api',
+  searchService: '/api/search/api',
+  notificationServiceHub: '/api/notification/notificationHub',
+
+  cloudinaryCloudName: 'du4qudomo',
+  cloudinaryUploadPresetName: 'booking-platform'
 };
