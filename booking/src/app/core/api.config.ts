9232b99564d16160fa4dd3c25f33a0c0a69260aa
Bogdan <bogdanjanosevic@gmail.com>
2026-02-07 14:12:16 +0100

    fix(apiconfig): Changed routes so it works with ingress

--- booking/src/app/core/api.config.ts
+++ booking/src/app/core/api.config.ts
@@ -1,11 +1,11 @@
 export const ApiConfig = {
-  userService: 'http://localhost:5001/api',
-  accommodationService: 'http://localhost:5002/api',
-  ratingService: 'http://localhost:5003/api',
-  reservationService: 'http://localhost:5004/api',
-  notificationService: 'http://localhost:5006/api',
-  notificationServiceHub: 'http://localhost:5006/notificationHub',
-  searchService: 'http://localhost:5005/api',
+  userService: '/api',
+  accommodationService: '/api',
+  ratingService: '/api',
+  reservationService: '/api',
+  notificationService: '/api',
+  notificationServiceHub: '/notificationHub',
+  searchService: '/api',
 
   cloudinaryCloudName: 'du4qudomo',
   cloudinaryUploadPresetName: 'booking-platform'
